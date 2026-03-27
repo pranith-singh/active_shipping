f7a4423739b887d0f7d7d2dce2f19da7edbb6d10
pranith-singh <thakurpranithraj@gmail.com>
2026-03-27 16:45:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
+# Home route
 @app.route("/")
 def home():
-    return "My app is live 🚀"
+    return "Shipping App is running 🚀"
 
-if __name__ == "__main__":
-    app.run()
+# Shipping cost calculator
+@app.route("/shipping", methods=["GET"])
+def shipping():
+    try:
+        weight = float(request.args.get("weight", 1))
+        distance = float(request.args.get("distance", 1))
+
+        cost = (weight * 5) + (distance * 0.5)
+
+        return jsonify({
+            "weight": weight,
+            "distance": distance,
+            "shipping_cost": cost
+        })
+
+    except:
+        return jsonify({"error": "Invalid input"})
